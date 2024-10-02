70ddd33f9ec2532e16ba6fd67ea977d3fe3da762
Ricardo-Gabriel <69832153+Ricardo-Gabriel@users.noreply.github.com>
2024-10-01 23:55:43 -0300

    Deixando mais bonitinho

--- 01desafio.js
+++ 01desafio.js
@@ -1,61 +1,40 @@
 const prompt = require("prompt-sync")();
 
 let listaHerois = [
-  "Ricardo:1000",
+  "Ricardo:100",
   "Bruno:2000",
   "Douglas:3000",
   "Renata:1000000",
 ];
 
-/**
-  let nome = prompt("Digite o nome do Heroi: ");
-  
-  let xp = parseInt(prompt("Digite a quantidade de Xp do Heroi: "));
-  console.log(xp);
-*/
-
-
 
 listaHerois.forEach((item) => {
-
-  let[nome,xp] = item.split(':');
-  CalculaNivel(nome,xp);
-
+  let [nome, xp] = item.split(":");
+  let nivel = CalculaNivel(xp);
+  console.log(Mensagem(nome, xp, nivel));
 });
 
+function Mensagem(nome, xp, nivel) {
+  let Mensagem = `O heroi ${nome}, tem ${xp} pontos de  experiencia e esta no nivel ${nivel}`;
+  return Mensagem;
+}
 
-function CalculaNivel(nome, xp) {
-  if (xp < 1000) {
-    console.log(
-      `O heroi ${nome}, tem ${xp} pontos de  experiencia e esta no nivel Ferro!`
-    );
+function CalculaNivel(xp) {
+  if (xp <= 1000) {
+    return "Ferro";
   } else if ((xp > 1000) & (xp <= 2000)) {
-    console.log(
-      `O heroi ${nome}, tem ${xp} pontos de  experiencia e esta no nivel Bronze!`
-    );
+    return "Bronze!";
   } else if ((xp > 2000) & (xp <= 5000)) {
-    console.log(
-      `O heroi ${nome}, tem ${xp} pontos de  experiencia e esta no nivel Prata!`
-    );
+    return "Prata!";
   } else if ((xp > 6000) & (xp <= 7000)) {
-    console.log(
-      `O heroi ${nome}, tem ${xp} pontos de  experiencia e esta no nivel Ouro!`
-    );
+    return " Ouro!";
   } else if ((xp > 7000) & (xp <= 8000)) {
-    console.log(
-      `O heroi ${nome}, tem ${xp} pontos de  experiencia e esta no nivel Platina!`
-    );
+    return "Platina!";
   } else if ((xp > 8000) & (xp <= 9000)) {
-    console.log(
-      `O heroi ${nome}, tem ${xp} pontos de  experiencia e esta no nivel Acendente!`
-    );
+    return "Acendente!";
   } else if ((xp > 9000) & (xp <= 10000)) {
-    console.log(
-      `O heroi ${nome}, tem ${xp} pontos de  experiencia e esta no nivel Imortal!`
-    );
-  } else {
-    console.log(
-      `O heroi ${nome}, tem ${xp} pontos de  experiencia e esta no nivel Radiante!`
-    );
+    return "Imortal!";
+  } else if (xp > 10000) {
+    return "Radiante!";
   }
 }
